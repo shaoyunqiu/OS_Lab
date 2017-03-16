30abbe3a078f275fb349b2d4be47f85be695a246
shaoyunqiu <shaoyunqiu@163.com>
2017-03-16 16:49:06 +0800

    add lab1 code

--- labcodes/lab2/kern/debug/kdebug.c
+++ labcodes/lab2/kern/debug/kdebug.c
@@ -305,5 +305,14 @@ print_stackframe(void) {
       *           NOTICE: the calling funciton's return addr eip  = ss:[ebp+4]
       *                   the calling funciton's ebp = ss:[ebp]
       */
+      uint32_t ebp = read_ebp() ;
+      uint32_t eip = read_eip() ;
+      for(int i = 0 ; ebp!= 0 && i < STACKFRAME_DEPTH ; i ++){
+        cprintf("ebp:0x%08x eip:0x%08x ", ebp, eip) ;
+        uint32_t *args = (uint32_t *)ebp + 2 ; //get the pointer.
+        cprintf("args: 0x%08x 0x%08x 0x%08x 0x%08x\n", args[0], args[1], args[2], args[3]) ;
+        print_debuginfo(eip-1) ;
+        eip = *((uint32_t *)ebp+1) ;
+        ebp = *((uint32_t *)ebp);
+      }
 }
-
--- labcodes/lab2/kern/trap/trap.c
+++ labcodes/lab2/kern/trap/trap.c
@@ -46,6 +46,14 @@ idt_init(void) {
       *     You don't know the meaning of this instruction? just google it! and check the libs/x86.h to know more.
       *     Notice: the argument of lidt is idt_pd. try to find it!
       */
+      extern uintptr_t __vectors[] ;
+      for(int i = 0 ; i < 256 ; i ++){
+        SETGATE(idt[i], 0, GD_KTEXT, __vectors[i], DPL_KERNEL) ;
+      }
+      //set for systecall, switch to kernel
+      SETGATE(idt[T_SWITCH_TOK], 1, GD_KTEXT, __vectors[T_SWITCH_TOK], DPL_USER) ;
+
+      lidt(&idt_pd) ;
 }
 
 static const char *
@@ -147,6 +155,8 @@ trap_dispatch(struct trapframe *tf) {
          * (2) Every TICK_NUM cycle, you can print some info using a funciton, such as print_ticks().
          * (3) Too Simple? Yes, I think so!
          */
+        ++ ticks ;
+        if(ticks % TICK_NUM == 0) print_ticks() ;
         break;
     case IRQ_OFFSET + IRQ_COM1:
         c = cons_getc();
@@ -158,8 +168,19 @@ trap_dispatch(struct trapframe *tf) {
         break;
     //LAB1 CHALLENGE 1 : YOUR CODE you should modify below codes.
     case T_SWITCH_TOU:
+        tf->tf_cs = USER_CS ; //CODE
+        tf->tf_ds = USER_DS ; //DATA
+        tf->tf_ss = USER_DS ; //STACKE
+        tf->tf_es = USER_DS ; //EXTRA
+        tf->tf_eflags |= (3 << 12) ; // enable I/O
+        //print_trapframe(tf) ;
+        break ;
     case T_SWITCH_TOK:
-        panic("T_SWITCH_** ??\n");
+        tf->tf_cs = KERNEL_CS ;
+        tf->tf_ds = KERNEL_DS ;
+        tf->tf_es = KERNEL_DS ;  
+        tf->tf_eflags &= ~(3 << 12) ; // disable I/0   
+        //panic("T_SWITCH_** ??\n");
         break;
     case IRQ_OFFSET + IRQ_IDE1:
     case IRQ_OFFSET + IRQ_IDE2:
